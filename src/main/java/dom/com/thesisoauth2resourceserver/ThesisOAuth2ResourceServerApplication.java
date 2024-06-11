64ff824f7968dac8531c2c3905e6553917364085
Octav <octavv2001@yahoo.com>
2024-06-11 20:48:35 +0300

    first commit

---
+++ src/main/java/dom/com/thesisoauth2resourceserver/ThesisOAuth2ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package dom.com.thesisoauth2resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisOAuth2ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThesisOAuth2ResourceServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dom/com/thesisoauth2resourceserver/ThesisOAuth2ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dom.com.thesisoauth2resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisOAuth2ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
